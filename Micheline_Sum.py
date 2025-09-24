f6d1f2d454b00ff93a472a7d658ef3912f635bb2
Micheline <g14460751.thu.edu.tw>
2025-09-24 16:50:21 +0800

    Create two files

--- Micheline_Sum.py
+++ Micheline_Sum.py
@@ -1,11 +1,15 @@
 #
 # Micheline, 2025/09/24
 # file: Micheline_Sum.py
-# Short description of the task
+# Add two numbers then print
 #
 
 # 1. Input
+X = 8
+Y = 7
 
 # 2. Process
+Sum = X + Y
 
 # 3. Output
+print(f'Result {Sum}')
